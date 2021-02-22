f4c27990d799c12bad1fade6b65d7ed30dfd7b79
adityak74 <akarnam37@gmail.com>
2021-02-21 19:13:11 -0600

    feat: scrape videos, comments for channel id

--- app/controllers/youtube_controller.rb
+++ app/controllers/youtube_controller.rb
@@ -1,23 +1,25 @@
 class YoutubeController < ActionController::API
+    VIDEO_LIMIT = 10
+    COMMENT_LIMIT = 20
     def scrapeChannel
         Yt.configuration.api_key = ENV['GOOGLE_API_KEY']
         begin
             channel = Yt::Channel.new id: params[:id]
-            render json: { status: 200, message: channel.title }
+            channelData = []
+            videos = channel.videos.limit VIDEO_LIMIT
+            for video in videos
+                video = Yt::Video.new id: video.id
+                comments = []
+                for commentThread in video.threads.limit COMMENT_LIMIT
+                    commentId = commentThread.id
+                    thread = Yt::CommentThread.new id: commentId
+                    comments.push(thread.top_level_comment.text_display)
+                end
+                channelData.push({ videoTitle: video.title, videoURL: video.canonical_url, comments: comments })
+            end
+            render json: { status: 200, channel: channel.title, channelId: channel.id, channelUrl: channel.canonical_url ,channelData: channelData }, status: 200
         rescue Exception => e
             render json: { status: 400, message: "invalid channel id" }, status: 400
         end
-        
-        # channel.title # => "Yt Test"
-        # video = Yt::Video.new id: "2zBp_715mJI"
-        # comments = []
-        # puts "Comments Size:", video.comment_count
-        # for commentThread in video.threads.limit 100
-        #     commentId = commentThread.id
-        #     puts commentId
-        #     thread = Yt::CommentThread.new id: commentId
-        #     comments.push(thread.top_level_comment.text_display)
-        # end
-        # render json: { status: 200, videoTitle: video.title, comments: comments }
     end
 end
